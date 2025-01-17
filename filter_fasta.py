3fa2b10947ab42ce78f947bde33165b31dc3f1a0
jobrant <jason.o.brant@gmail.com>
2025-01-17 14:56:59 -0500

    Update filter_fasta.py
    Changed so it filters based on reference length, not max aligned length. Updated all docstrings to Google's Python Style guide. Improved efficiency by changing how files parsed.

--- filter_fasta.py
+++ filter_fasta.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Created on Mon Jun 17 14:46:26 2019
+'''
+FASTA Alignment Filter
 
-@author: jasonbrant
-Run filter_fasta.py -h for help. 
-Use this to filter a fasta alignment file for use with methylmapper and Rhonda's R clustering scripts. 
-This will read an alignment fasta, determine the reference length, and the max length of the alignments. 
-This will then write out a new alignment fasta of those aligned reads that meet user defined length paramters.
-Command line argument 1 is the fasta file, argument 2 is percentage of max aligned read length. 
-So 90 would mean each read must be at least 90% the length of the max aligned read length. 
-By default, the output fasta file will be named like the input file, but ending in filtered.fa. 
-Optionally, you can specify an output file name using the -o option.
+This script filters FASTA alignment files for use with methylmapper and R clustering scripts.
+It filters aligned reads based on a minimum length requirement relative to the reference sequence.
 
-"""
+Usage:
+    filter_fasta.py <input_fasta> <percent_length> [-o OUTPUT]
+
+Arguments:
+    input_fasta     Input FASTA alignment file (output from bsDraw.py)
+    percent_length  Minimum percent length relative to reference sequence (e.g., 90)
+    -o, --output    Optional output filename (default: input_name.filtered.fa)
+
+Example:
+    filter_fasta.py input.fa 90 -o filtered_output.fa
+
+Created on: Mon Jun 17 14:46:26 2019
+Author: jasonbrant
+'''
 
 from Bio import SeqIO
 import argparse
 
-def read_fasta(fname):
+def read_fasta(fname: str) -> tuple[str, str]:
     '''
-    This function reads in FASTA file and returns a tuple containing the read ID and the sequence.
-    :param fname:
-    :return:
+    Reads in FASTA file and yields tuples of sequence IDs and sequences.
+    
+    Args:
+        fname (str): Path to input FASTA file
+        
+    Yields:
+        tuple[str, str]: Tuple containing the read ID and sequence
+        
+    Raises:
+        FileNotFoundError: If input file doesn't exist
     '''
+
     with open(fname) as fp:
         name, seq = None, []
         for line in fp:
@@ -37,56 +51,84 @@ def read_fasta(fname):
                 seq.append(line)
         if name: yield (name, ''.join(seq))
 
-def filter_seqs(filename, perc_len):
+def filter_seqs(filename: str, perc_len: float) -> set[str]:
     '''
-    This function determines the length of the reference sequence and filters out alignments that don't meet user supplied parameters.
-    :param filename:
-    :param perc_len:
-    :return:
+    This function determines the length of the reference sequence (first entry in FASTA) 
+    and filters out alignments that don't meet user supplied parameters based on the 
+    reference sequence length.
+    
+    Args:
+        filename (str): Path to the FASTA file
+        perc_len (float): Minimum percentage of reference length to keep
+
+    Returns:
+        set[str]: Set of sequence IDs that meet the length criteria
     '''
-    ref_len = 0
+
+    # Get reference length from first entry
+    first_entry = next(read_fasta(filename))  # Get first entry only
+    ref_len = len(first_entry[1])
+    
+    # Calculate minimum required length
+    min_required_length = ref_len * perc_len/100
+    
     alignment_lengths = []
     to_keep = set()
-    
-    for name, seq in read_fasta(filename):
-        if name.startswith(">"):
-            if not name.startswith(">m"):
-                ref_len = len(seq)
-        if name.startswith(">m"):
-            alignment_lengths.append(len(seq))
-    for name, seq in read_fasta(filename):
-        if len(seq) >= (max(alignment_lengths) * perc_len/100):
-            to_keep.add(name.strip('>'))
 
-    print("\nReference Length\t{}\n\nMax alignment length\t{}\nMin alignment length\t{}\n".format(ref_len, max(alignment_lengths), min(alignment_lengths)))
+    # Process remaining sequences
+    for name, seq in read_fasta(filename):
+        if name.startswith(">m"):  # Only process mapped sequences
+            seq_len = len(seq)
+            alignment_lengths.append(seq_len)
+            if seq_len >= min_required_length:
+                to_keep.add(name.strip('>'))
+
+    print("\nReference Length\t{}\n\nMax alignment length\t{}\nMin alignment length\t{}\n"
+          "Minimum required length ({}% of ref)\t{}\n"
+          .format(ref_len, max(alignment_lengths), min(alignment_lengths), 
+                 perc_len, min_required_length))
+    
+    print("Kept {} out of {} sequences".format(len(to_keep), len(alignment_lengths)))
     return to_keep
 
-def output_set(filename, nameset, output_name):
+def output_set(filename: str, nameset: set[str], output_name: str) -> None:
     '''
-    This function writes out the reference sequence and those alignments kept from filter_seqs.
-    :param filename:
-    :param nameset:
-    :param output_name:
-    :return:
+    Writes out the reference sequence and filtered alignments to a new FASTA file.
+    
+    Args:
+        filename (str): Path to input FASTA file
+        nameset (set[str]): Set of sequence IDs to keep
+        output_name (str): Path for output FASTA file
+        
+    Returns:
+        None
+        
+    Raises:
+        FileNotFoundError: If input file doesn't exist
+        PermissionError: If unable to write to output location
     '''
+
     seqiter = SeqIO.parse(open(filename), 'fasta')
     with open(output_name, "w+") as handle:
         SeqIO.write((seq for seq in seqiter if seq.id in nameset), handle, 'fasta')
 
-parser = argparse.ArgumentParser(description = "Run filter_fasta.py -h for help. Use this to filter a fasta alignment "
-                                               "file for use with methylmapper and Rhonda's R clustering scripts. "
-                                               "This will read an alignment fasta, determine the reference length, "
-                                               "and the max length of the alignments. This will then write out a new "
-                                               "alignment fasta of those aligned reads that meet user defined length "
-                                               "parameters. Command line argument 1 is the fasta file, argument 2 is "
-                                               "percentage of max aligned read length. So 90 would mean each read must "
-                                               "be at least 90% the length of the max aligned read length. By default, "
-                                               "the output fasta file will be named like the input file, but ending "
-                                               "in filtered.fa. Optionally, you can specify an output file name using the -o option. ")
+parser = argparse.ArgumentParser(
+    description="Filter FASTA alignments based on minimum length requirement relative to reference sequence."
+)
 
-parser.add_argument("filename", help = "Input an alignment FASTA file to be filtered. Should be the output of bsDraw.py")
-parser.add_argument("percent", help = "Percent max alignment length. Alignments below this amount are filtered out; e.g. 80 removes any alignments that are less than 80%% of the longest alignment.", type = float)
-parser.add_argument("-o", "--output", help = "Specify an optional output file name. Default name is base name of input with .filtered.fa appended.")
+parser.add_argument(
+    "filename", 
+    help="Input FASTA alignment file (output from bsDraw.py)"
+)
+parser.add_argument(
+    "percent", 
+    type=float,
+    help="Minimum percent length relative to reference sequence (e.g., 90)"
+)
+parser.add_argument(
+    "-o", "--output",
+    help="Optional output filename (default: input_name.filtered.fa)"
+)
         
 if __name__ == "__main__":
     args = parser.parse_args()
